e64fbacdb9c512bba78dad657bb8edcc098c4408
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2012-11-02 01:02:56 +0200

    Introducing the sign-in page supporting simple authentication scenarios. JUnit tests included.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,111 @@
 package controllers;
 
+import models.User;
 import play.*;
+import play.data.Form;
 import play.mvc.*;
 
 import views.html.*;
 
+/**
+ * The controller class for the application action methods.
+ *
+ * @author billy
+ *
+ */
 public class Application extends Controller {
-  
-  public static Result index() {
-    return ok(index.render("Your new application is ready."));
-  }
-  
+    // -- Home page
+
+    public static Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
+
+    // -- Sign-in / Log-in page
+
+    /**
+     * The inner class that backs the sign-in page.
+     *
+     * @author billy
+     *
+     */
+    public static class SignIn {
+        /**
+         * The e-mail to be authenticated.
+         */
+        public String email;
+
+        /**
+         * The password with which to authenticate the e-mail.
+         */
+        public String password;
+
+        /**
+         * <p>Validates the email and the password given.</p>
+         *
+         * <p>
+         * Uses the {@link models.User.authenticate()} helper method to
+         * authenticate the information given.
+         * </p>
+         *
+         * @return A {@code null} value if the validation passes or a
+         *         {@link String} with an error message, if it fails.
+         */
+        public String validate() {
+            String ret = null;
+            if (User.authenticate(email, password) == null) {
+                ret = "Invalid user or password";
+            }
+
+            return ret;
+        }
+    }
+
+    /**
+     * <p>The sign-in action.</p>
+     *
+     * <p>
+     * Constructs a {@code 200 OK} HTTP response, containing
+     * {@code app/views/signin.scala.html} as a body.
+     * </p>
+     *
+     * @return A {@code 200 OK} HTTP {@link Result}.
+     */
+    public static Result signIn() {
+        return ok(
+                signin.render(form(SignIn.class))
+        );
+    }
+
+    /**
+     * <p>The authentication action.</p>
+     *
+     * <p>
+     * Checks the submitted form for errors. If no errors were found it
+     * constructs a {@code 303 SEE_OTHER} HTTP response pointing to
+     * {@link Application.index()} action. In case of errors it prepares a
+     * {@code 400 BAD_REQUEST} HTTP response with
+     * {@code app/views/signin.scala.html} as body.
+     * </p>
+     *
+     * @return A {@code 303 SEE_OTHER} HTTP {@link Result} if the request has
+     *         no errors; a {@code 400 BAD_REQUEST} HTTP {@link Result}
+     *         otherwise.
+     */
+    public static Result authenticate() {
+        Result res = null;
+        // Create a form filled with the requested data.
+        Form<SignIn> form = form(SignIn.class).bindFromRequest();
+
+        if (form.hasErrors()) {
+            res = badRequest(signin.render(form));
+        }
+        else {
+            session().clear();
+            session("email", form.get().email);
+            res = redirect(
+                    routes.Application.index()
+            );
+        }
+        return res;
+    }
 }
---
+++ test/controllers/SignInTest.java
@@ -0,0 +1,58 @@
+package controllers;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.collect.ImmutableMap;
+
+import junit.framework.TestCase;
+
+import play.mvc.Result;
+import static play.test.Helpers.*;
+
+public class SignInTest extends TestCase {
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        start(fakeApplication(inMemoryDatabase()));
+        super.setUp();
+    }
+
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void testIt() {
+        Result res = null;
+
+        // -- Authenticate success
+        res = callAction(
+                controllers.routes.ref.Application.authenticate(),
+                fakeRequest().withFormUrlEncodedBody(ImmutableMap.of("email", "bob@example.com", "password", "secret"))
+        );
+
+        assertEquals(303, status(res));
+        assertEquals("bob@example.com", session(res).get("email"));
+
+        // -- Authenticate failure
+        res = callAction(
+                controllers.routes.ref.Application.authenticate(),
+                fakeRequest().withFormUrlEncodedBody(ImmutableMap.of("email", "bob@example.com", "password", "badpassword"))
+        );
+
+        assertEquals(400, status(res));
+        assertNull(session(res).get("email"));
+
+        res = callAction(
+                controllers.routes.ref.Application.authenticate(),
+                fakeRequest().withFormUrlEncodedBody(ImmutableMap.of("email", "unknown@example.com", "password", "secret"))
+        );
+
+        assertEquals(400, status(res));
+        assertNull(session(res).get("email"));
+    }
+}
