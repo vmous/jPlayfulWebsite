48854c8c5502af7bfa090f49ff99f884e2453314
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2013-03-10 10:47:36 +0200

    Setting up deployment on the Cloud with Heroku.

--- project/Build.scala
+++ project/Build.scala
@@ -18,7 +18,8 @@ object ApplicationBuild extends Build {
 //      filters,
       "org.webjars" % "webjars-play" % "2.1.0",
       "org.webjars" % "jquery" % "1.9.1",
-      "org.webjars" % "bootstrap" % "2.3.0"
+      "org.webjars" % "bootstrap" % "2.3.0",
+      "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
